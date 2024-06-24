90c0a7b50ec33d01242f9966538add00c9607ffd
Hanagami3 <hanaga3mi@gmail.com>
2024-06-24 15:30:53 +0200

    126 Handling form Submissions: Repetition

--- src/app/dashbord/server-status/server-status.component.ts
+++ src/app/dashbord/server-status/server-status.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-server-status',
@@ -7,18 +8,30 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, AfterViewInit /*, OnDestroy*/ {
   // c'est bien d'être plus précis et donc de créer un type spécial qui ne peut prendre qu'une de ces trois valeurs
   // comme ça, si un problème de typing aec l'écriture, directement soulevé par l'ide
   // ==> Literal Types
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+ 
+  // Avec cela on peut créer un listener qui déclenchera une fonction peu importe le cnponent dans lequel on inject DestroyRef
+  private destroyRef = inject(DestroyRef) 
 
+  
+  // private interval?: NodeJS.Timeout
+  // on reçoit une erreur donc utiliséer la syntaxe suivante qui est du ts avancé
+  //private interval?: ReturnType<typeof setInterval>
+
+
+  //constructorutilisé seulement pour y passer de simples classe mais pas pour des choses complexe (bad practice)
   constructor(){}
   // c'est mieux d'utiliser ngOnInit pour initialiser le travail comme setting up this interval
   //>< au contstructor, Angular is done initialzing the component
   // donc si mon component reçoi des valeur entrée, ces valeur seront initialisée
   ngOnInit() {
-    setInterval(() => {
+    console.log('ON INIT')
+    //this.interval = setInterval(() => {
+    const interval = setInterval(() => {
       const rnd = Math.random(); // 0 - 0,999999...
       if (rnd < 0.5) {
         this.currentStatus = 'online'
@@ -28,8 +41,22 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown'
       }
     }, 5000)
+    this.destroyRef.onDestroy(() =>{
+      clearInterval(interval)
+    })
   }
   //!\ au nom de la classe car opas d'erreur si on se trompe dans les majuscules par exemple (vu que c'est ok de créer ses propres méthode
   // mais du coup, angulare ne pourra pas la cibler) 
   //Mais on peut implementer OnInit pour forcer la classe a avoir cette bonne méthode
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT')
+  }
+
+  //On peut avoir des fuite de memoir avec interval donc utiliser OnDestroy pour nettoyer cela
+  // on est sur que cette interval est nettoyé si le component est supprimé
+  //ancienne manière mais qui fonctionne sur les anciennes versions
+  // ngOnDestroy(){
+  //     clearTimeout(this.interval)
+  // }
 }
--- src/app/dashbord/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashbord/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
+//Pour permettre ngSubmit dans l'html
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-new-ticket',
     standalone: true,
     templateUrl: './new-ticket.component.html',
     styleUrl: './new-ticket.component.css',
-    imports: [ControlComponent, ButtonComponent]
+    imports: [ControlComponent, ButtonComponent, FormsModule]
 })
 export class NewTicketComponent {
-
+    onSubmit(){
+        console.log('SUBMITTED!')
+    }
 }
